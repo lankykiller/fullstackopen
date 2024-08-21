9b7d8030403219598006583f1aa7765c12d23d15
lankykiller <dani.mwai@gmail.com>
2024-08-21 19:37:26 +0300

    tehtava1

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,22 +1,54 @@
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+ 
 
-  return(
-    <div>
-      <h1>Greetings</h1>
-      <Hello/>
-    </div>
-  )
-}
-
-const Hello = () => {
   return (
     <div>
-      <p>Hello world</p>
+      <h1><Header header={course}/></h1>
+      <p>
+        <Content part={part1} exercises={exercises1}/>
+      </p>
+      <p>
+        <Content part={part2} exercises={exercises2}/>
+      </p>
+      <p>
+        <Content part={part3} exercises={exercises3}/>
+      </p>
+      <p>
+        <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      </p>
     </div>
   )
 }
+
+const Header = (props) => {
+  return (
+    <div>
+      <p>{props.header}</p>
+    </div>
+  )
+}
+
+const Content =(props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
+}
+
 export default App
