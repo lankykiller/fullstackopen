9d0104344059c3a14b3ed037cf23b2196de447ef
lankykiller <dani.mwai@gmail.com>
2024-09-03 16:11:38 +0300

    1c

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,24 +1,40 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const nimi = 'Pekka'
-  const ika = 10
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text="plus" />
+      <Button handleClick={setToZero} text="zero" />
+      <Button handleClick={decreaseByOne} text="minus" />
     </div>
   )
-}
+} 
+
+const Display = ({counter}) => <div>{counter}</div>
+
+
+const Button = ({handleClick, text}) => 
+    <button onClick={handleClick}>
+      {text}
+    </button>
 
 export default App
