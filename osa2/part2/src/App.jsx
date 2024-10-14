156ef117c9cd8e1494c23ea5a85d115f84725fef
lankykiller <dani.mwai@gmail.com>
2024-10-14 19:15:14 +0300

    har 2e

--- osa2/part2/src/App.jsx
+++ osa2/part2/src/App.jsx
@@ -5,8 +5,52 @@ import Notification from './components/Notification'
 import Footer from './components/Footer'
 
 
+import axios from 'axios'
+
 const App = () => {
-  const [notes, setNotes] = useState([])
+  const [value, setValue] = useState('')
+  const [rates, setRates] = useState({})
+  const [currency, setCurrency] = useState(null)
+
+  useEffect(() => {
+    console.log('effect run, currency is now', currency)
+
+    // skip if currency is not defined
+    if (currency) {
+      console.log('fetching exchange rates...')
+      axios
+        .get(`https://open.er-api.com/v6/latest/${currency}`)
+        .then(response => {
+          setRates(response.data.rates)
+        })
+    }
+  }, [currency])
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const onSearch = (event) => {
+    event.preventDefault()
+    setCurrency(value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        currency: <input value={value} onChange={handleChange} />
+        <button type="submit">exchange rate</button>
+      </form>
+      <pre>
+        {JSON.stringify(rates, null, 2)}
+      </pre>
+    </div>
+  )
+}
+
+
+/* const App = () => {
+  const [notes, setNotes] = useState(null)
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState('some error happened...')
@@ -18,6 +62,10 @@ const App = () => {
         setNotes(initialNotes)
       })
   }, [])
+
+  if (!notes) { 
+    return null 
+  }
 
   const addNote = (event) => {
     event.preventDefault()
@@ -90,7 +138,7 @@ const App = () => {
       <Footer />
     </div>
   )
-}
+} */
 
 
 export default App
