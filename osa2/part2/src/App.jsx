77bef591042014d37437cad94568efcceaf2de87
lankykiller <dani.mwai@gmail.com>
2024-11-04 14:39:42 +0200

    osa2

--- osa2/part2/src/App.jsx
+++ osa2/part2/src/App.jsx
@@ -4,56 +4,11 @@ import noteService from './services/notes'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
 
-
-import axios from 'axios'
-
 const App = () => {
-  const [value, setValue] = useState('')
-  const [rates, setRates] = useState({})
-  const [currency, setCurrency] = useState(null)
-
-  useEffect(() => {
-    console.log('effect run, currency is now', currency)
-
-    // skip if currency is not defined
-    if (currency) {
-      console.log('fetching exchange rates...')
-      axios
-        .get(`https://open.er-api.com/v6/latest/${currency}`)
-        .then(response => {
-          setRates(response.data.rates)
-        })
-    }
-  }, [currency])
-
-  const handleChange = (event) => {
-    setValue(event.target.value)
-  }
-
-  const onSearch = (event) => {
-    event.preventDefault()
-    setCurrency(value)
-  }
-
-  return (
-    <div>
-      <form onSubmit={onSearch}>
-        currency: <input value={value} onChange={handleChange} />
-        <button type="submit">exchange rate</button>
-      </form>
-      <pre>
-        {JSON.stringify(rates, null, 2)}
-      </pre>
-    </div>
-  )
-}
-
-
-/* const App = () => {
   const [notes, setNotes] = useState(null)
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
+  const [errorMessage, setErrorMessage] = useState('')
 
   useEffect(() => {
     noteService
@@ -138,7 +93,50 @@ const App = () => {
       <Footer />
     </div>
   )
-} */
+} 
 
 
 export default App
+
+import axios from 'axios'
+
+/*const App = () => {
+  const [value, setValue] = useState('')
+  const [rates, setRates] = useState({})
+  const [currency, setCurrency] = useState(null)
+
+  useEffect(() => {
+    console.log('effect run, currency is now', currency)
+
+    // skip if currency is not defined
+    if (currency) {
+      console.log('fetching exchange rates...')
+      axios
+        .get(`https://open.er-api.com/v6/latest/${currency}`)
+        .then(response => {
+          setRates(response.data.rates)
+        })
+    }
+  }, [currency])
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const onSearch = (event) => {
+    event.preventDefault()
+    setCurrency(value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        currency: <input value={value} onChange={handleChange} />
+        <button type="submit">exchange rate</button>
+      </form>
+      <pre>
+        {JSON.stringify(rates, null, 2)}
+      </pre>
+    </div>
+  )
+} */
