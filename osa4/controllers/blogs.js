93753ae901fea9d8f1f696638891b878a353b11f
lankykiller <dani.mwai@gmail.com>
2025-09-26 12:01:51 +0300

    4d

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,8 +1,10 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs);
 });
 
@@ -18,11 +20,30 @@ blogsRouter.get("/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
 
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+
+  const user = await User.findById(decodedToken.id)
+
   if (!body.title || !body.url) {
     return response.status(400).json({ error: "title and url are required" });
+  }
+
+    if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
   }
 
   const blog = new Blog({
@@ -30,9 +51,13 @@ blogsRouter.post("/", async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user._id
   });
 
   const newBlog = await blog.save();
+  user.blogs = user.blogs.concat(newBlog._id)
+  await user.save()
+
   response.status(201).json(newBlog);
 });
 
